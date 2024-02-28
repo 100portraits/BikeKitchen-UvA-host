50f9d77c4f8dcbc3695aa390d8bf3f4f86e066fa
sahir <sahirde@gmail.com>
2024-02-28 22:22:55 +0100

    add link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class='flex flex-col h-screen m-auto w-screen justify-center items-center gap-3'>
     <p>BK host app</p>
-    <a href="https://sahir.nl" class="underline decoration-1">sahir.nl</p>
+    <a href="https://sahir.nl" class="underline decoration-1">sahir.nl</a>
     <a href="/appointment" class="underline text-xl">View prototype</a>
 </div>
