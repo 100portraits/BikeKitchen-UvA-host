13ea8ad41c8f0d8e70fa26510a4ed2463ed6cc9b
sahir <sahirde@gmail.com>
2024-02-28 22:20:16 +0100

    add link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<div class='flex flex-col h-screen m-auto w-screen justify-center items-center'>
-    BK host app
+<div class='flex flex-col h-screen m-auto w-screen justify-center items-center gap-3'>
+    <p>BK host app</p>
+    <a href="/appointment">View prototype</a>
 </div>
